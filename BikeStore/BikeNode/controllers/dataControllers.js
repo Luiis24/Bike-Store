d78fd034651c4ff2626be67c1931635329057208
Luis <134552997+Luiis24@users.noreply.github.com>
2024-07-05 00:28:56 -0500

    Update dataControllers.js
    Se borro el codigo comentado

--- BikeStore/BikeNode/controllers/dataControllers.js
+++ BikeStore/BikeNode/controllers/dataControllers.js
@@ -58,30 +58,7 @@ const getProducts = (req, res)=> {
             })
         });
     };
-    // const loginUser = async (req, res) => {
-    //     const { email, password } = req.body;
-    
-    //     if (!email || !password) {
-    //         return res.status(400).json({ error: 'Falta informacion requerida' });
-    //     }
-    //     try {
-    //         const result = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
-    //         if (result.rows.length === 0) {
-    //             return res.status(401).json({ error: 'Usuario no encontrado' });
-    //         }
-    //         const user = result.rows[0];
-    
-    //         const passwordMatch = await bcrypt.compare(password, user.password);
-    //         if (!passwordMatch) {
-    //             return res.status(401).json({ error: 'Contraseña incorrecta' });
-    //         }
-    //         res.status(200).json({ message: 'Inicio de sesión exitoso' });
-    //     } catch (error) {
-    //         console.error('Error al consultar la base de datos', error);
-    //         return res.status(500).json({ error: 'Error al iniciar sesión' });
-    //     }
-    // };
-    
+
     const loginUser = async (req, res) => {
         const { email, password } = req.body;
     
